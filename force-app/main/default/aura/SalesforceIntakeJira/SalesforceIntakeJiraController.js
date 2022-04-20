ebdaac406d7d1fddd0adeefeb0b119c9f4070a17
Bhimisetty Venkata Chaitanya <bhimisetty.venkatachaitanya@rackspace.com>
2022-04-20 20:05:08 +0530

    2ndcommit

---
+++ force-app/main/default/aura/SalesforceIntakeJira/SalesforceIntakeJiraController.js
@@ -0,0 +1,7 @@
+({
+	openIntakeJiraBoard : function(component, event, helper) {        
+        
+        var urlval = 'https://rackspace.atlassian.net/jira/software/c/projects/INTAKE/boards/1951';
+        window.open(urlval, '_blank');
+    }
+})
